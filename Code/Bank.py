08df6b893f503bb4363f9769f38b54f61f409194
Sulaiman <ahamedsulaimaan2003@gmail.com>
2025-02-04 15:15:39 +0530

    Deleted unwanted files

---
+++ Code/Bank.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}. New balance: {self.balance}")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.balance}")
+        else:
+            print("Insufficient balance!")
+
+
+# Creating an account object
+account = BankAccount("Ahamed",1000)
+
+# Performing transactions
+account.deposit(500)
+account.withdraw(300)
+account.withdraw(2000)

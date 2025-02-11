5aa6a20ac52e7f7db49839cd5d065abb7a7dc3a5
Sulaiman <ahamedsulaimaan2003@gmail.com>
2025-02-11 17:20:52 +0530

    Newly added

--- Sulaiman-04022025/Cricket_game.py
+++ Sulaiman-04022025/Cricket_game.py
@@ -7,7 +7,8 @@ game_on=True
 
 while game_on:
     player= int(input("Enter a number from 1 to 5: "))
-    computer=random.randint(1,6)
+    computer=random.randint(1,5)
+    print(f"Player number = {player}  & Computer Number = {computer}")
     if player==computer:
         print(f"Game Over")
         game_on=False
@@ -15,33 +16,38 @@ while game_on:
         print("Enter below 6")
     else:
         player_score+=player
-
-print(f"The score is {player_score}")
+        print(f"Player Score:{player_score}")
+print(f"The player score is {player_score}")
 computer_game_on=True
 
 #Computer Batting
 
 while computer_game_on:
     player= int(input("Enter a number from 1 to 5: "))
-    computer=random.randint(1,6)
+    computer=random.randint(1,5)
+    print(f"Player number = {player}  & Computer Number = {computer}")
+
     if player==computer:
         print(f"Game Over")
         computer_game_on=False
     elif player>5:
         print("Enter below 6")
     else:
-        computer_score+=player
+        computer_score+=computer
+        print(f"Computer Score:{computer_score}")
         if computer_score>player_score:
+            # computer_score+=
             computer_game_on=False
 
 
-#Result
 
+#Result
+print(f"The computer score is {computer_score}")
 if player_score > computer_score:
     print("You win")
 elif player_score == computer_score:
     print("draw")
 else:
     print("You Loose")
-print(f"The score is {computer_score}")
+
 

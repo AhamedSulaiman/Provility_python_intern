766d7edb5a68552a2d7f4e87ac1db98bf617295b
Sulaiman <ahamedsulaimaan2003@gmail.com>
2025-02-14 16:30:54 +0530

    Newly added

--- Sulaiman-14022025/eval_func.py
+++ Sulaiman-14022025/eval_func.py
@@ -2,6 +2,7 @@ list1="[x**2 for x in range(1,6)]"
 
 print(eval(list1))
 print()
+
 #Calling the function dynamically
 
 def square(n):
@@ -10,6 +11,7 @@ def square(n):
 result = eval("square(7)")
 print(result)
 print()
+
 
 expr = "5 * (10 + 2) / 4"
 result = eval(expr)
